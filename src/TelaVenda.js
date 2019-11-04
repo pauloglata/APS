4544cb35cc0286603b842b992f39ca3c69f516b5
pauloglata <47419814+pauloglata@users.noreply.github.com>
2019-11-03 23:01:28 -0300

    Pronto
    Pronto

--- src/TelaVenda.js
+++ src/TelaVenda.js
@@ -1,43 +1,117 @@
-package Visão;
+package teste;
 
-public class TelaVenda extends javax.swing.JFrame {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
+public class TelaVenda extends JFrame implements ActionListener {
+	
+	private JButton add;
+	private JButton search;
+	private JButton edit;
+	private JButton delete;
+	
+	private JSeparator bottom;
+	private JSeparator rightSide;
+	private JSeparator leftSide;
+	private JSeparator upperMinor;
+	private JSeparator upper;
+	
+	private JTextField text;
+	private JLabel title;
+	
+	private JTable tableVendas;
+	private JScrollPane scrollPainel;
+	private JLabel logo;
 
     public TelaVenda() {
         initComponents();
     }
 
-
     @SuppressWarnings("unchecked")
     private void initComponents() {
-
-        jLabel1 = new javax.swing.JLabel();
-        jLabeNomeTela = new javax.swing.JLabel();
-
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTableVendas = new javax.swing.JTable();
-        jButtonExcluir = new javax.swing.JButton();
-        jButtonAdicionar = new javax.swing.JButton();
-        jButtonBuscar = new javax.swing.JButton();
-        jTextFieldBuscar = new javax.swing.JTextField();
-        jButtonEditar = new javax.swing.JButton();
-        jSeparatorMiniSuperior = new javax.swing.JSeparator();
-        jSeparatorLateralEsquerda = new javax.swing.JSeparator();
-        jSeparatorSuperior = new javax.swing.JSeparator();
-        jSeparatorLateralDireita = new javax.swing.JSeparator();
-        jSeparatorInferior = new javax.swing.JSeparator();
-        jLabelTelaPrincipal = new javax.swing.JLabel();
-
-        jLabel1.setText("jLabel1");
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    	
+    	// Home layout null for background image 
         getContentPane().setLayout(null);
+    	// End 
+    	
+    	// Home add
+    	add = new JButton();
+    	add.setText("ADICIONAR");
+        getContentPane().add(add);
+        add.setBounds(370, 380, 110, 40);
+    	// End add
+    	
+    	// Home search
+        search = new JButton();
+        search.setText("BUSCAR");
+        getContentPane().add(search);
+        search.setBounds(500, 380, 90, 40);
+    	// End search
+    	
+    	// Home edit
+        edit = new javax.swing.JButton();
+        edit.setText("EDITAR");
+        getContentPane().add(edit);
+        edit.setBounds(90, 380, 100, 40);
+    	// End edit
+    	
+    	// Home delete
+        delete = new javax.swing.JButton();
+        delete.setText("EXCLUIR");
+        getContentPane().add(delete);
+        delete.setBounds(230, 380, 90, 40);
+    	// End delete
+    	
+    	// Home bottom
+        bottom = new javax.swing.JSeparator();
+        getContentPane().add(bottom);
+        bottom.setBounds(20, 360, 830, 20);
+    	// End bottom
+    	
+    	// Home rightSide
+        rightSide = new javax.swing.JSeparator();
+        rightSide.setOrientation(javax.swing.SwingConstants.VERTICAL);
+        getContentPane().add(rightSide);
+        rightSide.setBounds(850, 30, 10, 330);
+    	// End rightSide
+    	
+    	// Home leftSide
+        leftSide = new javax.swing.JSeparator();
+        leftSide.setOrientation(javax.swing.SwingConstants.VERTICAL);
+        getContentPane().add(leftSide);
+        leftSide.setBounds(20, 30, 10, 330);
+    	// End leftSide
+    	
+    	// Home upperMinor
+        upperMinor = new javax.swing.JSeparator();
+        getContentPane().add(upperMinor);
+        upperMinor.setBounds(20, 30, 10, 10);
+    	// End upperMinor
+    	
+    	// Home upper
+        upper = new javax.swing.JSeparator();
+        getContentPane().add(upper);
+        upper.setBounds(90, 30, 760, 10);
+    	// End  upper
+    	
+    	// Home text
+        text = new javax.swing.JTextField();
+        getContentPane().add(text);
+        text.setBounds(610, 390, 180, 30);
+    	// End text
+    	
+    	// Home title
+        title = new javax.swing.JLabel();
+        title.setText("VENDAS");
+        getContentPane().add(title);
+        title.setBounds(40, 20, 60, 20);
+    	// End title
+    	
+    	// Home tableVendas
+        tableVendas = new JTable();
 
-        jLabeNomeTela.setText("VENDAS");
-        getContentPane().add(jLabeNomeTela);
-        jLabeNomeTela.setBounds(40, 20, 60, 20);
-
-        jTableVendas.setModel(new javax.swing.table.DefaultTableModel(
+        tableVendas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
             {null, null, null, null, null},
             {null, null, null, null, null},
@@ -70,91 +144,56 @@ public class TelaVenda extends javax.swing.JFrame {
             "ID", "Nome", "CPF", "Telefone", "Data de Nascimento"
         }
     ));
-        jScrollPane1.setViewportView(jTableVendas);
-
-        getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(60, 60, 750, 280);
-
-        jButtonExcluir.setText("EXCLUIR");
-        getContentPane().add(jButtonExcluir);
-        jButtonExcluir.setBounds(230, 380, 90, 40);
-
-        jButtonAdicionar.setText("ADICIONAR");
-        getContentPane().add(jButtonAdicionar);
-        jButtonAdicionar.setBounds(370, 380, 90, 40);
-
-        jButtonBuscar.setText("BUSCAR");
-        getContentPane().add(jButtonBuscar);
-        jButtonBuscar.setBounds(500, 380, 90, 40);
-        getContentPane().add(jTextFieldBuscar);
-        jTextFieldBuscar.setBounds(610, 390, 180, 30);
-
-        jButtonEditar.setText("EDITAR");
-        getContentPane().add(jButtonEditar);
-        jButtonEditar.setBounds(90, 380, 100, 40);
-        getContentPane().add(jSeparatorMiniSuperior);
-        jSeparatorMiniSuperior.setBounds(20, 30, 10, 10);
-
-        jSeparatorLateralEsquerda.setOrientation(javax.swing.SwingConstants.VERTICAL);
-        getContentPane().add(jSeparatorLateralEsquerda);
-        jSeparatorLateralEsquerda.setBounds(20, 30, 10, 330);
-        getContentPane().add(jSeparatorSuperior);
-        jSeparatorSuperior.setBounds(90, 30, 760, 10);
-
-        jSeparatorLateralDireita.setOrientation(javax.swing.SwingConstants.VERTICAL);
-        getContentPane().add(jSeparatorLateralDireita);
-        jSeparatorLateralDireita.setBounds(850, 30, 10, 330);
-        getContentPane().add(jSeparatorInferior);
-        jSeparatorInferior.setBounds(20, 360, 830, 20);
-
-        jLabelTelaPrincipal.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Tela-princiapal.PNG"))); // NOI18N
-        jLabelTelaPrincipal.setIconTextGap(5);
-        getContentPane().add(jLabelTelaPrincipal);
-        jLabelTelaPrincipal.setBounds(0, 0, 860, 458);
-
-        pack();
-    }// </editor-fold>
+        // End tableVendas
+    	
+    	// Home scrollPainel
+        scrollPainel = new javax.swing.JScrollPane();
+        scrollPainel.setViewportView(tableVendas);
+        getContentPane().add(scrollPainel);
+        scrollPainel.setBounds(60, 60, 750, 280);
+    	// End scrollPainel
+    	
+    	// Home logo
+        logo = new javax.swing.JLabel();
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Tela-princiapal.PNG"))); // NOI18N
+        logo.setIconTextGap(5);
+        getContentPane().add(logo);
+        logo.setBounds(0, 0, 860, 458);
+    	// End logo
+    }
 
 
     public static void main(String args[]) {
 
-    try {
-    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-    if ("Nimbus".equals(info.getName())) {
-    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-    break;
-}
-}
-} catch (ClassNotFoundException ex) {
-    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-} catch (InstantiationException ex) {
-    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-} catch (IllegalAccessException ex) {
-    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//    try {
+//    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//    if ("Nimbus".equals(info.getName())) {
+//    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//    break;
+//}
+//}
+//} catch (ClassNotFoundException ex) {
+//    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//} catch (InstantiationException ex) {
+//    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//} catch (IllegalAccessException ex) {
+//    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//    java.util.logging.Logger.getLogger(TelaVenda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//}
+
+    	TelaVenda tve = new TelaVenda();
+    	tve.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    	tve.pack();
+		tve.setSize(876, 498);
+    	tve.setVisible(true);
 }
 
-java.awt.EventQueue.invokeLater(new Runnable() {
-    public void run() {
-        new TelaVenda().setVisible(true);
-    }
-});
-}
 
-private javax.swing.JButton jButtonAdicionar;
-private javax.swing.JButton jButtonBuscar;
-private javax.swing.JButton jButtonEditar;
-private javax.swing.JButton jButtonExcluir;
-private javax.swing.JLabel jLabeNomeTela;
-private javax.swing.JLabel jLabel1;
-private javax.swing.JLabel jLabelTelaPrincipal;
-private javax.swing.JScrollPane jScrollPane1;
-private javax.swing.JSeparator jSeparatorInferior;
-private javax.swing.JSeparator jSeparatorLateralDireita;
-private javax.swing.JSeparator jSeparatorLateralEsquerda;
-private javax.swing.JSeparator jSeparatorMiniSuperior;
-private javax.swing.JSeparator jSeparatorSuperior;
-private javax.swing.JTable jTableVendas;
-private javax.swing.JTextField jTextFieldBuscar;
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
